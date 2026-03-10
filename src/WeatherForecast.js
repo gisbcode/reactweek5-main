764dae2e4d01e083cd2e7b5c668939f678686170
Gigi <creative.giovannasb@gmail.com>
2026-03-10 22:00:14 +0000

    fixedweatherforecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,8 +3,7 @@ import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherDay from "./WeatherDay";
 
-export default function WeatherForecast({ submittedCity }) {
-  const [submittedCity, setSubmittedCity] = useState("");
+export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
@@ -28,16 +27,19 @@ export default function WeatherForecast({ submittedCity }) {
                   <WeatherDay data={dailyForecast} />
                 </div>
               );
-            } else return null;
+            }
+            return null;
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "39a3014fd34afe90bc14c4tc7oed280d";
+    let apiKey = "YOUR_API_KEY";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${submittedCity}&key=${apiKey}`;
+
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+
     axios.get(apiUrl).then(handleResponse);
 
     return null;
