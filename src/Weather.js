59579ab6a78c6fe546351fa19788b7ff4b8f41a9
Gigi <creative.giovannasb@gmail.com>
2026-03-10 21:39:52 +0000

    fixing points

--- src/Weather.js
+++ src/Weather.js
@@ -10,16 +10,20 @@ export default function Weather(props) {
 
   function handleResponse(response) {
     setWeatherData({
-      ready: true, 
-      coordinates: response.data.coord,
-      temperature: response.data.temperature.current
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      ready: true,
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      icon: response.data.condition.icon_url,
     });
+  }
+
+  function search() {
+    const apiKey = "39a3014fd34afe90bc14c4tc7oed280d";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${submittedCity}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleSubmit(event) {
@@ -31,38 +35,20 @@ export default function Weather(props) {
     setCity(event.target.value);
   }
 
-  function search() {
-    const apiKey = "39a3014fd34afe90bc14c4tc7oed280d";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${submittedCity}&key=${apiKey}`;
-    
-    axios.get(apiUrl).then(handleResponse);
-  }
-
   if (weatherData.ready) {
     return (
       <div className="Weather">
         <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            onChange={handleCityChange}
+          />
+          <input type="submit" value="Search" />
         </form>
+
         <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData.coordinates} />
+        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
