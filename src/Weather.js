17ccd9df16d95b05730b8cdbe8fbc7876c1bcffc
Gigi <creative.giovannasb@gmail.com>
2026-03-10 21:49:30 +0000

    const submitted city

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+   const [submittedCity, setSubmittedCity] = useState(""); 
 
   function handleResponse(response) {
     setWeatherData({
